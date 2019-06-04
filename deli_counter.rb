ba9a0d63b13f2cbbe7a03e760bf90e750c02a333
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 18:17:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ if array.length >= 1
   line = []
   counter = 1
   array.each do |name|
-    nuarray.push("#{counter}. #{name}")
+    line.push("#{counter}. #{name}")
     counter += 1
   end
   puts "The line is currently: #{line.join(" ")}"
