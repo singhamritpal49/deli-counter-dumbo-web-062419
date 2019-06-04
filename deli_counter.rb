72bc66a215e665c82e0f7d0fc2f973fe617eadb4
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 18:14:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,26 +6,15 @@ puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
-#  def line(katz_deli)
-#    if katz_deli.length == 0
-#      puts "The line is currently empty."
-#    else
-#      line = []
-#      katz_deli.each_with_index do { |name,index| line.push() }
-#end
-  #end
-  def line_method(numinline)
-    line_method_array = []
-    if numinline.length == 0
+ def line(katz_deli)
+   if katz_deli.length == 0
       puts "The line is currently empty."
     else
-      line_method_array.each.with_index(0) do |name, index|
-        line_method_array.push("#{index}. #{name}")
-        # push method will push the name into the index
-      end
-      puts "The line is: #{line_method_array.join(" ")}"
-    end
+      line = []
+      katz_deli.each_with_index do { |name,index| line.push() }
+end
   end
+
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
