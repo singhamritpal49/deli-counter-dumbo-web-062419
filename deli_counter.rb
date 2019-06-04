addc275f502e46095318fc07f074fe07173c37c3
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 18:15:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,8 +10,7 @@ end
     if katz_deli.length == 0
       puts "The line is currently empty."
     else
-      line = []
-      katz_deli.each_with_index do { |name,index| line.push }
+      puts "hello"
 end
   end
 
