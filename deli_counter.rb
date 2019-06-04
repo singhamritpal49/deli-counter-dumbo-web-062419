f42ba790ff13105552b3476ff5478a4d2b26e9c6
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 18:17:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,13 +8,13 @@ end
 
 def line(array) # this was the one I figured out
 if array.length >= 1
-  nuarray = []
+  line = []
   counter = 1
   array.each do |name|
     nuarray.push("#{counter}. #{name}")
     counter += 1
   end
-  puts "The line is currently: #{nuarray.join(" ")}"
+  puts "The line is currently: #{line.join(" ")}"
 else
   puts "The line is currently empty."
 end
