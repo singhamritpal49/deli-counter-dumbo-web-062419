331ee50fa3b7c7c1859cd856752ceedbd1151f69
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 18:19:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,18 +6,18 @@ puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
-def line(katz_deli)
-if katz_deli.length >= 1
-  line = []
-  counter = 1
-  katz_deli.each do |name|
-    katz_deli.push("#{counter}. #{name}")
-    counter += 1
+def line(katz_deli) # this was the one I figured out
+  if katz_deli.length >= 1
+    line = []
+    counter = 1
+    katz_deli.each do |name|
+      line.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{line.join(" ")}"
+  else
+    puts "The line is currently empty."
   end
-  puts "The line is currently: #{line.join(" ")}"
-else
-  puts "The line is currently empty."
-end
 end
 
 def now_serving(katz_deli)
