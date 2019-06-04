7a050a466371918bd2335a1f3a86f2c8c9ae9703
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 17:52:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   elsif katz_deli.length >1
     puts "Currently serving #{katz_deli.first}."
-
+    katz_deli.shift
 
 end
 end
