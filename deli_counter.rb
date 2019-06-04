2ee68110f018ac5190de818cdf3f1bdee132346b
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 17:30:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,9 +20,11 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length > 0
-  katz_deli.each do |name|
+    puts "There is nobody waiting to be served!"
+  elsif
+    katz_deli.each do |name|
      puts   "Currently serving #{name}."
      katz_deli.shift
 end
-puts
+
 end
