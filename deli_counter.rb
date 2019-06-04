5908e40b131ecb61e4591a989fe1b1303f771aa8
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 18:17:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,13 +6,19 @@ puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
-  def line(katz_deli)
-    if katz_deli.length == 0
-      puts "The line is currently empty."
-    else
-      puts "hello"
+def line(array) # this was the one I figured out
+if array.length >= 1
+  nuarray = []
+  counter = 1 
+  array.each do |name|
+    nuarray.push("#{counter}. #{name}")
+    counter += 1 
+  end 
+  puts "The line is currently: #{nuarray.join(" ")}"
+else
+  puts "The line is currently empty."
 end
-  end
+end
 
 def now_serving(katz_deli)
   if katz_deli.length == 0
